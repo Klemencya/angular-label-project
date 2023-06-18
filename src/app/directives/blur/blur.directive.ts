0cca766a7bb44531ecfdf26a1cc40ec6d30b94aa
ritasidorska <m.sidorskaya@innopolis.university>
2023-06-18 17:13:32 +0400

    Осталось только понять, как добавить спиннер

--- src/app/directives/blur/blur.directive.ts
+++ src/app/directives/blur/blur.directive.ts
@@ -1,24 +1,59 @@
-import {Directive, ElementRef, Input} from "@angular/core";
+import {Directive, ElementRef, Input, Renderer2, TemplateRef, ViewChild, ViewContainerRef} from "@angular/core";
 
 @Directive({
   selector: '[blurDirective]'
 })
 
 export class BlurDirective {
+  private _blur: boolean
+
+  constructor(private el: ElementRef,
+              private template: TemplateRef<any>,
+              private container: ViewContainerRef,
+              private renderer: Renderer2) {}
+
+  // @ViewChild(this.template, {static: false}) childElement: ElementRef;
+
   @Input()
-  public get blur() {
+  public set blurDirective(blurStatus: boolean) {
+    this._blur = blurStatus;
+
+
+      // const div = this.renderer.createElement('div');
+      // this.renderer.setStyle(div, 'filter', 'blur(5px)');
+      // this.renderer.appendChild(div, this.template.elementRef.nativeElement);
+      // this.renderer.appendChild(this.el.nativeElement, div);
+    if (this._blur) {
+      this.container.clear()
+
+      this.container.insert(this.template.createEmbeddedView(null))
+      const element = (this.container.get(0) as any).rootNodes[0]
+      this.renderer.setStyle(element, 'filter', 'blur(5px)');
+      this.renderer.setStyle(element, 'pointerEvents', 'none');
+      // this.renderer.appendChild(element, this.template.elementRef.nativeElement);
+      // console.log(this.container.element.nativeElement)
+      // console.log(this.template.elementRef.nativeElement)
+      // this.renderer.setStyle(this.template.elementRef.nativeElement, 'filter', 'blur(5px)')
+    } else {
+      this.container.clear()
+      this.container.insert(this.template.createEmbeddedView(null))
+      const element = (this.container.get(0) as any).rootNodes[0]
+      this.renderer.setStyle(element, 'filter', 'blur(0)');
+      this.renderer.setStyle(element, 'pointerEvents', 'auto');
+      // this.renderer.appendChild(element, this.template.elementRef.nativeElement);
+      // this.container.clear()
+      // this.container.createEmbeddedView(this.template)
+    }
+
+    // if (this._blur) {
+      // this.el.nativeElement.style.filter = 'blur(4px)';
+      // this.el.nativeElement.style.pointerEvents = 'none';
+    // } else {
+      // this.el.nativeElement.style.filter = 'blur(0)';
+      // this.el.nativeElement.style.pointerEvents = 'auto';
+    // }
+  }
+  public get blurDirective() {
     return this._blur
   }
-  public set blur(arg: boolean) {
-    this._blur = arg
-    if (this._blur) {
-      this.el.nativeElement.style.filter = 'blur(4px)';
-      this.el.nativeElement.style.pointerEvents = 'none';
-    } else {
-      this.el.nativeElement.style.filter = 'blur(0)';
-      this.el.nativeElement.style.pointerEvents = 'auto';
-    }
-  }
-  private _blur: boolean
-  constructor(private el: ElementRef) {}
 }
