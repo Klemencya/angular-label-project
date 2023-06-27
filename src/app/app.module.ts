27f746492f36fc78d7a37739384e342d365b751b
ritasidorska <m.sidorskaya@innopolis.university>
2023-06-27 10:48:06 +0400

    doci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,8 @@
+import { provideSearchEngine, NgDocDefaultSearchEngine, NgDocModule } from "@ng-doc/app";
+import { NG_DOC_ROUTING, NgDocGeneratedModule } from "@ng-doc/generated";
+import { RouterModule } from "@angular/router";
+import { NgDocSidebarModule } from "@ng-doc/app/components/sidebar";
+import { NgDocNavbarModule } from "@ng-doc/app/components/navbar";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -28,8 +33,13 @@ import {DialogService} from "primeng/dynamicdialog";
     CarouselModule,
     ImageModule,
     BrowserAnimationsModule,
-  ],
-  providers: [DialogService],
+      NgDocNavbarModule,
+      NgDocSidebarModule,
+      RouterModule.forRoot(NG_DOC_ROUTING, {scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', scrollOffset: [0, 70]}),
+      NgDocModule.forRoot(),
+      NgDocGeneratedModule.forRoot()
+],
+  providers: [DialogService, provideSearchEngine(NgDocDefaultSearchEngine)],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/docs/label/ng-doc.page.ts
@@ -0,0 +1,8 @@
+import {NgDocPage} from '@ng-doc/core';
+
+const LabelPage: NgDocPage = {
+	title: `Label`,
+	mdFile: './index.md',
+};
+
+export default LabelPage;
